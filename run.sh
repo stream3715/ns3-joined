cc4b39fcbacb1c9d24485b63d301c2c61b213804
Shohei Okubo <stream.larn3@gmail.com>
2022-06-08 00:42:21 +0900

    run.shの環境名指定必須化

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     echo "指定された引数は$#個です。" 1>&2
-    echo "実行するには1個の引数が必要です。" 1>&2
+    echo "実行するには2個の引数が必要です。" 1>&2
     exit 1
 fi
 
-./waf --run=ndn-kademlia >$1.log
+./waf --run=ndn-kademlia >$2.log
 
 if [ $? -ne 0 ]; then
     echo "シミュレーションに失敗"
@@ -14,12 +13,17 @@ if [ $? -ne 0 ]; then
     exit -1
 fi
 
-mkdir -p result/$1
-rm -rf result/$1/*
-mv app-delays-trace.tsv drop-trace.tsv rate-trace.tsv $1.log result/$1/.
-cd result/$1
-/home/strea/t2c.sh app-delays-trace.tsv
-/home/strea/t2c.sh drop-trace.tsv
-/home/strea/t2c.sh rate-trace.tsv
+mkdir -p result/$1/$2
+rm -rf result/$1/$2/*
+
+mv app-delays-trace.tsv result/$1/$2/app-delays-trace-$2.tsv
+mv drop-trace.tsv result/$1/$2/drop-trace-$2.tsv
+mv rate-trace.tsv result/$1/$2/rate-trace-$2.tsv
+mv $2.log result/$1/$2/$2.log
+
+cd result/$1/$2
+/home/strea/t2c.sh app-delays-trace-$2.tsv
+/home/strea/t2c.sh drop-trace-$2.tsv
+/home/strea/t2c.sh rate-trace-$2.tsv
 
 exit 0
