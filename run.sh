362d0090a4a14f1d73d8affb9d3aba28169269e8
Shohei Okubo <stream.larn3@gmail.com>
2022-08-02 21:56:19 +0900

    可変長でのConsumer設定対応

--- run.sh
+++ run.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     echo "指定された引数は$#個です。" 1>&2
-    echo "実行するには2個の引数が必要です。" 1>&2
+    echo "実行するには3個の引数が必要です。" 1>&2
     exit 1
 fi
 
-export ID_PRD=37
-export ID_CON_MJ1=10
-export ID_CON_MJ2=21
-export ID_CON_MJ3=27
-export ID_CON_MN1=8
-export ID_CON_MN2=16
-export ID_CON_MN3=33
+export ID_PRD=$1
+export ID_CON_MJ=$2
+export ID_CON_MN=$3
 
-./waf --run=ndn-kademlia >$2.log
+./waf --run=ndn-kademlia >$1-$2-$3.log
 
 if [ $? -ne 0 ]; then
     echo "シミュレーションに失敗"
@@ -21,17 +17,17 @@ if [ $? -ne 0 ]; then
     exit -1
 fi
 
-mkdir -p result/$1/$2
-rm -rf result/$1/$2/*
+mkdir -p result-2/$1/$2-$3
+rm -rf result-2/$1/$2-$3/*
 
-mv app-delays-trace.tsv result/$1/$2/k-app-delays-trace-$2.tsv
-mv drop-trace.tsv result/$1/$2/k-drop-trace-$2.tsv
-mv rate-trace.tsv result/$1/$2/k-rate-trace-$2.tsv
-mv $2.log result/$1/$2/k-$2.log
+mv app-delays-trace.tsv result-2/$1/$2-$3/k-app-delays-trace-$1-$2-$3.tsv
+mv drop-trace.tsv result-2/$1/$2-$3/k-drop-trace-$1-$2-$3.tsv
+mv rate-trace.tsv result-2/$1/$2-$3/k-rate-trace-$1-$2-$3.tsv
+mv $1-$2-$3.log result-2/$1/$2-$3/k-$1-$2-$3.log
 
-cd result/$1/$2
-/home/strea/t2c.sh k-app-delays-trace-$2.tsv
-/home/strea/t2c.sh k-drop-trace-$2.tsv
-/home/strea/t2c.sh k-rate-trace-$2.tsv
+cd result-2/$1/$2-$3
+/home/strea/t2c.sh k-app-delays-trace-$1-$2-$3.tsv
+/home/strea/t2c.sh k-drop-trace-$1-$2-$3.tsv
+/home/strea/t2c.sh k-rate-trace-$1-$2-$3.tsv
 
 exit 0
