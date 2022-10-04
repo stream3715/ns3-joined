a566e167b422a46f5433986c77c1486e7668d95d
Shohei Okubo <stream.larn3@gmail.com>
2022-10-04 20:45:39 +0900

    prefix指定を追加

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
     echo "指定された引数は$#個です。" 1>&2
     echo "実行するには3個の引数が必要です。" 1>&2
     exit 1
 fi
 
-export ID_PRD=$1
-export ID_CON_MJ=$2
-export ID_CON_MN=$3
+export ID_PRD=$2
+export ID_CON_MJ=$3
+export ID_CON_MN=$4
 
-./waf --run=ndn-kademlia >$1-$2-$3.log
+./waf --run=ndn-kademlia > $2-$3-$4.log
 
 if [ $? -ne 0 ]; then
     echo "シミュレーションに失敗"
@@ -17,17 +17,17 @@ if [ $? -ne 0 ]; then
     exit -1
 fi
 
-mkdir -p result-2ml/$1/$2-$3
-rm -rf result-2ml/$1/$2-$3/*
+mkdir -p result-$1/$2/$3-$4
+rm -rf result-$1/$2/$3-$4/*
 
-mv app-delays-trace.tsv result-2ml/$1/$2-$3/k-2ml-app-delays-trace-$1-$2-$3.tsv
-mv drop-trace.tsv result-2ml/$1/$2-$3/k-2ml-drop-trace-$1-$2-$3.tsv
-mv rate-trace.tsv result-2ml/$1/$2-$3/k-2ml-rate-trace-$1-$2-$3.tsv
-mv $1-$2-$3.log result-2ml/$1/$2-$3/k-2ml-$1-$2-$3.log
+mv app-delays-trace.tsv result-$1/$2/$3-$4/k-$1-app-delays-trace-$2-$3-$4.tsv
+mv drop-trace.tsv result-$1/$2/$3-$4/k-$1-drop-trace-$2-$3-$4.tsv
+mv rate-trace.tsv result-$1/$2/$3-$4/k-$1-rate-trace-$2-$3-$4.tsv
+mv $2-$3-$4.log result-$1/$2/$3-$4/k-$1-$2-$3-$4.log
 
-cd result-2ml/$1/$2-$3
-/home/strea/t2c.sh k-2ml-app-delays-trace-$1-$2-$3.tsv
-/home/strea/t2c.sh k-2ml-drop-trace-$1-$2-$3.tsv
-/home/strea/t2c.sh k-2ml-rate-trace-$1-$2-$3.tsv
+cd result-$1/$2/$3-$4
+/home/strea/t2c.sh k-$1-app-delays-trace-$2-$3-$4.tsv
+/home/strea/t2c.sh k-$1-drop-trace-$2-$3-$4.tsv
+/home/strea/t2c.sh k-$1-rate-trace-$2-$3-$4.tsv
 
 exit 0
